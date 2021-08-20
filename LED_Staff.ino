d19b473e8760fdf455499f2ad5180d1c0dc9d2da
Austin Poulson <austinpoulson@gmail.com>
2021-08-19 21:36:43 -0500

    Documentation, fill mode

--- LED_Staff.ino
+++ LED_Staff.ino
@@ -6,7 +6,7 @@
 int isClicked = isClicked;
 bool initMode = initMode;
 int mode = mode;
-int MODECOUNT = 7;
+int MODECOUNT = 8;
 int r = r;
 int g = g;
 int b = b;
@@ -49,6 +49,16 @@ void checkButtons() {
   }
 }
 
+//******************************* Utility Functions ******************************* 
+void blackout() {
+  for(int i = 0; i < LED_COUNT; i++){
+    leds0[i] = CRGB(0, 0, 0);
+    leds1[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+}
+
+//********************************* Mode Functions ********************************* 
 void dopsRGB() { //Dops RGB (Flash 3)
   if (initMode == 1){
     r = 255;
@@ -87,6 +97,14 @@ void dopsRGB() { //Dops RGB (Flash 3)
   }
   FastLED.show();
   delay(100);
+}
+
+void dops(int red1, int green1, int blue1, int red2, int green2, int blue2, int red3, int green3, int blue3, int red4, int green4, int blue4, int red5, int green5, int blue5, int red6, int green6, int blue6, int red7, int green7, int blue7, int red8, int green8, int blue8, int red9, int green9, int blue9, int red10, int green10, int blue10) {
+  if (initMode == 1){
+    blackout();
+    cycle = 0;
+    initMode = 0;
+  }
 }
 
 void rainbowFade() { //Rainbow fade (fade 7)
@@ -266,11 +284,107 @@ void sinWave() {
   delay(50);
 }
 
-void slip() {
+void slip() { //WIP
   if (initMode == 1){
     initMode = 0;
     cycle = 0;
   }
+}
+
+void fill(int red, int green, int blue) {
+  if (initMode == 1){
+    r = red;
+    g = green;
+    b = blue;
+    initMode = 0;
+    cycle = 0;
+  }
+  if (cycle >= LED_COUNT) {
+    blackout();
+    cycle = 0;
+  }
+  leds0[cycle] = CRGB(r, g, b);
+  leds1[cycle] = CRGB(r, g, b);
+  cycle++;
+  FastLED.show();
+  delay(1);
+}
+
+void fillBounce(int red, int green, int blue) { //WIP
+  if (initMode == 1){
+    r = red;
+    g = green;
+    b = blue;
+    initMode = 2;
+    cycle = 0;
+  }
+  switch(initMode) {
+    case 2:
+      leds0[cycle] = CRGB(r, g, b);
+      leds1[cycle] = CRGB(r, g, b);
+      cycle++;
+      if (cycle >= LED_COUNT) {
+        initMode = 3;
+        cycle = 0;
+      }
+      break;
+    case 3:
+      leds0[cycle] = CRGB(0, 0, 0);
+      leds1[cycle] = CRGB(0, 0, 0);
+      cycle++;
+      if (cycle >= LED_COUNT) {
+        initMode = 2;
+        cycle = 0;
+      }
+      break;
+  }
+  FastLED.show();
+  delay(1);
+}
+
+void fillFive(int red1, int green1, int blue1, int red2, int green2, int blue2, int red3, int green3, int blue3, int red4, int green4, int blue4, int red5, int green5, int blue5) {
+  if (initMode == 1){
+    blackout();
+    initMode = 0;
+    cycle = 0;
+  }
+  if (cycle >= LED_COUNT) {
+    blackout();
+    cycle = 0;
+  }
+  int line = cycle/5;
+  switch(line) {
+    case 1:
+      r = red1;
+      g = green1;
+      b = blue1;
+      break;
+    case 2:
+      r = red2;
+      g = green2;
+      b = blue2;
+      break;
+    case 3:
+      r = red3;
+      g = green3;
+      b = blue3;
+      break;
+    case 4:
+      r = red4;
+      g = green4;
+      b = blue4;
+      break;
+    case 5:
+      r = red5;
+      g = green5;
+      b = blue5;
+      break;
+  }
+  leds0[cycle] = CRGB(r, g, b);
+  leds1[cycle] = CRGB(r, g, b);
+  cycle++;
+  FastLED.show();
+  delay(1);
 }
 
 void loop() {
@@ -297,6 +411,12 @@ void loop() {
       break;
     case 6:
       sinWave(); //currently broken
+      break;
+    case 7:
+      fill(50, 50, 150);
+      break;
+    case 8:
+      fillFive(150, 0, 0, 0, 150, 0, 0, 0, 150, 150, 0, 150, 150, 150, 0);
       break;
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LED_Staff
 This is the code I wrote for my custom built LED staff. The code is running on an Arduino Trinket using Neopixels (WS2812B).
+
+Documentation:
+
+To use this project, you need to either change the code to work with your project, or message me for the schematics and follow them.
+
+The idea in this project is to run my custom built led staff while also developing a framework to make it easier to build led patterns. 
